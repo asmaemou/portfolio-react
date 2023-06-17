5cf224353c7ebc230fe8ae8d997c23adc15d0288
Asmae Mouradi <amouradi2002@gmail.com>
2023-06-17 15:37:45 -0700

    resolve errors

--- portfolioreact/src/App.js
+++ portfolioreact/src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import {Routes, Route} from 'react-router-dom'
-import Navbar from './Navbar';
-import Home from './Home';
-import About from './About';
-import Services from './Services';
-import Portfolio from './Portfolio';
-import Contact from './Contact';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import About from "./About";
+import Services from "./Services";
+import Portfolio from "./Portfolio";
+import Contact from "./Contact";
 
-function App (){
+function App() {
   return (
     <>
       <Navbar />
@@ -20,5 +20,5 @@ function App (){
       </Routes>
     </>
   );
-};
+}
 export default App;
