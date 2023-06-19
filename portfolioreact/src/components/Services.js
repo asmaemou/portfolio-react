ff66a8ce261f9de5b66025ca21123443d6ff5ec9
Asmae Mouradi <amouradi2002@gmail.com>
2023-06-19 16:51:33 -0700

    move my logo to the right+add the icons of my services

--- portfolioreact/src/components/Services.js
+++ portfolioreact/src/components/Services.js
@@ -1,33 +1,49 @@
-const Services =() =>{
-    return(
-        <>
-        <div id="services">
-   <div className="container">
-      <h1 className="sub-title">My services</h1>
-      <div className="services-list">
-         <div>
-            <i className="fa-solid fa-code"></i>
-            <h2>Web Design</h2>
-            <p>I learned how to create a user-friendly websites. I make sure to satisfy those important point such as: Custom Website Design,Responsive Design,User experience optimization,Visual branding integration,Collaborative Approach</p>
-            <a><i className="fa fa-phone" aria-hidden="true"></i> your text</a>
-
-         </div>
-         <div>
-            <i className="fa-solid fa-crop-simple"></i>
-            <h2>UI/UX design</h2>
-            <p>Elevate your digital experience with our UI/UX Design Services. We combine user-centric design principles and captivating visuals to create intuitive interfaces that engage and delight the audience.</p>
-            <a href="/#"></a>
-         </div>
-         <div>
-            <i className="fa-brands fa-app-store"></i>
-            <h2>APP Design</h2>
-            <p>Transform your vision into a stunning mobile experience with our App Design services. We blend seamless usability and captivating aesthetics to create intuitive interfaces that bring your app to life.</p>
-            <a href="/#"></a>
-         </div>
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCode, faCropSimple} from "@fortawesome/free-solid-svg-icons";
+import {faAppStore } from "@fortawesome/free-brands-svg-icons"
+const Services = () => {
+  return (
+    <>
+      <div id="services">
+        <div className="container">
+          <h1 className="sub-title">My services</h1>
+          <div className="services-list">
+            <div>
+              <FontAwesomeIcon icon={faCode} />
+              <h2>Web Design</h2>
+              <p>
+                I learned how to create a user-friendly websites. I make sure to
+                satisfy those important point such as: Custom Website
+                Design,Responsive Design,User experience optimization,Visual
+                branding integration,Collaborative Approach
+              </p>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faCropSimple} />
+              <h2>UI/UX design</h2>
+              <p>
+                Elevate your digital experience with our UI/UX Design Services.
+                We combine user-centric design principles and captivating
+                visuals to create intuitive interfaces that engage and delight
+                the audience.
+              </p>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faAppStore}/>
+              <h2>APP Design</h2>
+              <p>
+                Transform your vision into a stunning mobile experience with our
+                App Design services. We blend seamless usability and captivating
+                aesthetics to create intuitive interfaces that bring your app to
+                life.
+              </p>
+              {/* <Link to="/">Link Text</Link> */}
+            </div>
+          </div>
+        </div>
       </div>
-   </div>
-</div>
-        </>
-    );
-}
+    </>
+  );
+};
 export default Services;
