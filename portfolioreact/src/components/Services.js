fdb21af571633e5fdb89b109d6012eafb02a7382
Asmae Mouradi <amouradi2002@gmail.com>
2023-06-19 17:07:49 -0700

    saving all new changes

--- portfolioreact/src/components/Services.js
+++ portfolioreact/src/components/Services.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCode, faCropSimple} from "@fortawesome/free-solid-svg-icons";
 import {faAppStore } from "@fortawesome/free-brands-svg-icons"
