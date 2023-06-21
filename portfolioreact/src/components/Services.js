21f6fc1df7a13b3e6a70cc5d7cbd5c773ceda370
Asmae Mouradi <amouradi2002@gmail.com>
2023-06-21 12:08:57 -0700

    adding the hook in services useState

--- portfolioreact/src/components/Services.js
+++ portfolioreact/src/components/Services.js
@@ -1,43 +1,65 @@
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCode, faCropSimple} from "@fortawesome/free-solid-svg-icons";
-import {faAppStore } from "@fortawesome/free-brands-svg-icons"
+import { faCode, faCropSimple } from "@fortawesome/free-solid-svg-icons";
+import { faAppStore } from "@fortawesome/free-brands-svg-icons";
+
 const Services = () => {
+  const [showWebDesignDescription, setShowWebDesignDescription] = useState(false);
+  const [showDataStructureDescription, setShowDataStructureDescription] = useState(false);
+  const [showAPPDesign, setAPPDesign] = useState(false);
+
+  const handleWebDesignClick = () => {
+    setShowWebDesignDescription(!showWebDesignDescription);
+  };
+
+  const handleDataStructureClick = () => {
+    setShowDataStructureDescription(!showDataStructureDescription);
+  };
+
+  const handleAPPDesign = () => {
+    setAPPDesign(!showAPPDesign);
+  };
+
   return (
     <>
       <div id="services">
         <div className="container">
           <h1 className="sub-title">My services</h1>
           <div className="services-list">
-            <div>
+            <div onClick={handleWebDesignClick}>
               <FontAwesomeIcon icon={faCode} />
               <h2>Web Design</h2>
-              <p>
-                I learned how to create a user-friendly websites. I make sure to
-                satisfy those important point such as: Custom Website
-                Design,Responsive Design,User experience optimization,Visual
-                branding integration,Collaborative Approach
-              </p>
+              {showWebDesignDescription && (
+                <p>
+                  - Create user-friendly websites.
+                  <br />
+                  - Custom Website Design.
+                  <br />
+                  - Responsive Design.
+                  <br />
+                  - User experience optimization.
+                </p>
+              )}
             </div>
-            <div>
+            <div onClick={handleDataStructureClick}>
               <FontAwesomeIcon icon={faCropSimple} />
-              <h2>UI/UX design</h2>
-              <p>
-                Elevate your digital experience with our UI/UX Design Services.
-                We combine user-centric design principles and captivating
-                visuals to create intuitive interfaces that engage and delight
-                the audience.
-              </p>
+              <h2>Data Structure and Algorithms</h2>
+              {showDataStructureDescription && (
+                <p>
+                  - Gain knowledge of different data structures.
+                  <br />
+                  - Learn how to use data structures to solve coding problems.
+                </p>
+              )}
             </div>
-            <div>
-              <FontAwesomeIcon icon={faAppStore}/>
-              <h2>APP Design</h2>
-              <p>
-                Transform your vision into a stunning mobile experience with our
-                App Design services. We blend seamless usability and captivating
-                aesthetics to create intuitive interfaces that bring your app to
-                life.
-              </p>
-              {/* <Link to="/">Link Text</Link> */}
+            <div onClick={handleAPPDesign}>
+              <FontAwesomeIcon icon={faAppStore} />
+              <h2>App Design</h2>
+              {showAPPDesign && (
+                <p>
+                  - Design applications based on client requirements.
+                </p>
+              )}
             </div>
           </div>
         </div>
@@ -45,4 +67,5 @@ const Services = () => {
     </>
   );
 };
+
 export default Services;
